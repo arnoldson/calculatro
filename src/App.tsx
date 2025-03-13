eb70e2bf57927bd56fb091b7581bbb16824c51e8
arnoldson <arnold.son@icloud.com>
2025-03-13 11:24:23 -0700

    Added modals for draw select UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react"
 import { test } from "./calculator/test"
+import { DrawSelect } from "./components/DrawSelect"
 
 function App() {
   test()
   return (
     <>
       <h1>Calculatro</h1>
+      <DrawSelect />
     </>
   )
 }
