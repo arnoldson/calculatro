64274db3432584f40bac194bd960d474e4821209
arnoldson <arnold.son@icloud.com>
2025-03-14 12:05:00 -0700

    removed unused imports and added package for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react"
-import { test } from "./calculator/test"
 import { DrawSelect } from "./components/DrawSelect"
 
 function App() {
-  test()
   return (
     <>
       <h1>Calculatro</h1>
